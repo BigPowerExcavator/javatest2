342bd9e4756a03ddd903171cf106b42208ae2681
jaydon.liang <jaydon.liang@crop.to8to.com>
2021-01-13 09:28:39 +0800

    first

---
+++ src/main/java/com/test/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.test.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @PostMapping("/test1")
+    public String test1(){
+        return "test1";
+    }
+
+    @PostMapping("/test2")
+    public String test2(){
+        return "test2";
+    }
+
+
+}
