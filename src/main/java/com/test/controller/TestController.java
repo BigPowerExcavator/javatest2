bcf93911fa3621038e0ded7754d168c8d294cd47
jaydon.liang <jaydon.liang@crop.to8to.com>
2021-01-13 10:08:32 +0800

    init

---
+++ src/main/java/com/test/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.test.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @PostMapping("/test1")
+    public String test1(){
+        return "test1";
+    }
+
+    @PostMapping("/test2")
+    public String test2(){
+        System.out.println("这里修改了代码");
+        return "test2";
+    }
+
+
+}
